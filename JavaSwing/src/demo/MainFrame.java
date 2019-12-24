5fc6aff0e0139426222ec2d76aad3a78b0ec2995
Dilruba Khan <rubatanisa@yahoo.ie>
2019-12-24 20:21:10 +0000

    Comunication between components

--- JavaSwing/src/demo/MainFrame.java
+++ JavaSwing/src/demo/MainFrame.java
@@ -9,28 +9,27 @@ import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
 	private static final long serialVersionUID = 1L;
+	
+	private Toolbar toolbar;
 	private TextPannel textPanel;
-	private JButton btn;
+
 	
 	public MainFrame () {
 		super("Hello World!");
 		
 		setLayout(new BorderLayout());
 		
+		
+		toolbar = new Toolbar();
 		textPanel = new TextPannel();
-		btn = new JButton("Click Me");
 		
-		btn.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				textPanel.appendTex("hello \n");
-			}
-		});
-		
+		toolbar.setTextPanel(textPanel);
+	
+		add(toolbar, BorderLayout.NORTH);
 		add(textPanel, BorderLayout.CENTER);
-		add(btn, BorderLayout.SOUTH);
 		
-		setSize(600, 600);
+		
+		setSize(600, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
--- JavaSwing/src/demo/Toolbar.java
+++ JavaSwing/src/demo/Toolbar.java
@@ -1,22 +1,47 @@
 package demo;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Toolbar extends JPanel {
+public class Toolbar extends JPanel implements ActionListener {
 	private JButton helloBtn;
 	private JButton goodByBtn;
+	
+	private TextPannel textPanel;
 	
 	public Toolbar() {
 		helloBtn = new JButton("Hello");
 		goodByBtn = new JButton("Goodby");
+		
+		helloBtn.addActionListener(this);
+		goodByBtn.addActionListener(this);	
 		
 		setLayout(new FlowLayout(FlowLayout.LEFT));
 		
 		add(helloBtn);
 		add(goodByBtn);
 	}
+	
+	public void setTextPanel(TextPannel textPanel) {
+		this.textPanel = textPanel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton clicked = (JButton) e.getSource();
+		if(clicked == helloBtn) {
+			textPanel.appendTex("Hello\n");
+		}
+		else if(clicked == goodByBtn) {
+			textPanel.appendTex("Goodby\n");
+		}
+		
+	}
+	
+	
 
 }
