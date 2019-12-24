f1ed694c5c7a52204fe9594c0f69e6a7ac36e3fc
Dilruba Khan <rubatanisa@yahoo.ie>
2019-12-24 00:36:15 +0000

    Add Action listener to the button

--- JavaSwing/src/demo/MainFrame.java
+++ JavaSwing/src/demo/MainFrame.java
@@ -1,6 +1,8 @@
 package demo;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,6 +20,13 @@ public class MainFrame extends JFrame{
 		
 		textArea = new JTextArea();
 		btn = new JButton("Click Me");
+		
+		btn.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("hello \n");
+			}
+		});
 		
 		add(textArea, BorderLayout.CENTER);
 		add(btn, BorderLayout.SOUTH);
