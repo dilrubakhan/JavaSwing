0cd959131580834fa0c0e844e179f26678776450
Dilruba Khan <rubatanisa@yahoo.ie>
2019-12-24 01:22:16 +0000

    Modifing MainFrame

--- JavaSwing/src/demo/App.java
+++ JavaSwing/src/demo/App.java
@@ -6,10 +6,7 @@ public class App {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
-				
+			public void run() {			
 				new MainFrame();
 			}
 		});
--- JavaSwing/src/demo/MainFrame.java
+++ JavaSwing/src/demo/MainFrame.java
@@ -6,11 +6,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame{
-	
-	private JTextArea textArea;
+	private static final long serialVersionUID = 1L;
+	private TextPannel textPanel;
 	private JButton btn;
 	
 	public MainFrame () {
@@ -18,17 +17,17 @@ public class MainFrame extends JFrame{
 		
 		setLayout(new BorderLayout());
 		
-		textArea = new JTextArea();
+		textPanel = new TextPannel();
 		btn = new JButton("Click Me");
 		
 		btn.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
-				textArea.append("hello \n");
+				textPanel.appendTex("hello \n");
 			}
 		});
 		
-		add(textArea, BorderLayout.CENTER);
+		add(textPanel, BorderLayout.CENTER);
 		add(btn, BorderLayout.SOUTH);
 		
 		setSize(600, 600);
@@ -36,6 +35,4 @@ public class MainFrame extends JFrame{
 		setVisible(true);
 	}
 	
-	
-
 }
