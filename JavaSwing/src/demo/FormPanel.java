e137132042b229b6a3bbb4d1d073d673bc81dcb2
Dilruba Khan <rubatanisa@yahoo.ie>
2019-12-27 21:38:09 +0000

    Set GridBugLayout in FormPanel

--- JavaSwing/src/demo/FormPanel.java
+++ JavaSwing/src/demo/FormPanel.java
@@ -1,20 +1,97 @@
 package demo;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
+	
+	private JLabel nameLabel;
+	private JLabel occupationalLabel;
+	private JTextField nameField;
+	private JTextField occupationField;
+	private JButton okBtn;
 
 	public FormPanel() {
 		Dimension dim = getPreferredSize();
 		dim.width=250;
 		setPreferredSize(dim);
+		
+		nameLabel = new JLabel("Name: ");
+		occupationalLabel = new JLabel("Occupation: ");
+		nameField = new JTextField(10);
+		occupationField = new JTextField(10);
+		
+		okBtn = new JButton("OK");
+		
 		Border innerBorder = BorderFactory.createTitledBorder("Add Person");
 		Border outerBorder = BorderFactory.createEmptyBorder(5,5,5,5);
 		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+		
+		setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+		
+		
+		
+		
+		///////////////First Row/////////////////
+		
+		gc.weightx = 1;
+		gc.weighty = 0.1;
+		
+		gc.gridx = 0;
+		gc.gridy = 0;
+
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.LINE_END;
+		
+		//name field and nameLabel
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(nameLabel, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 0;
+		gc.anchor = GridBagConstraints.LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(nameField, gc);
+		
+		///////////////Second Row/////////////////
+		
+		//Occupation field and label
+		
+		gc.weightx = 1;
+		gc.weighty = 0.1;
+		
+		gc.gridy = 1;
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(occupationalLabel, gc);
+		
+		gc.gridy = 1;
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(occupationField, gc);
+		
+		///////////////Third Row/////////////////
+		//OK Button
+		gc.weightx = 1;
+		gc.weighty = 2.0;
+		gc.gridy = 2;
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(okBtn, gc);
+		
 	}
 }
